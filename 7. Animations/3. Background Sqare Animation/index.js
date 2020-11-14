2a43f6fb646e086038c1f4858b85d98f0bf32dbb
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-11-14 12:49:33 +0100

    Background Square Animation

---
+++ 7. Animations/3. Background Sqare Animation/index.js
@@ -0,0 +1,29 @@
+const colors = [
+    '#2196f3',
+    '#e91e63',
+    '#ffef3f',
+    '#74dd13'
+]
+
+function createSquare() {
+    const section = document.querySelector('section')
+    const square = document.createElement('span')
+
+    let size = Math.random() * 50
+
+    square.style.width = 20 + size + 'px'
+    square.style.height = 20 + size + 'px'
+
+    square.style.top = Math.random() * innerHeight + 'px'
+    square.style.left = Math.random() * innerWidth + 'px'
+
+    square.style.background = colors[Math.floor(Math.random() * colors.length)]
+
+    section.appendChild(square)
+
+    setTimeout(() => {
+        square.remove()
+    }, 5000)
+}
+
+setInterval(createSquare, 150)
